be160102ac6a524fbd47abcd964aa4fd993802f8
lgyong511 <lgyong511@163.com>
2024-03-24 14:44:24 +0800

    测试默认配置创建loging和使用配置文件创建loging

--- main/main.go
+++ main/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"loging"
+	"os"
 )
 
 var log *loging.Loging
 
-func initF() {
-	fmt.Println("初始化函数开始工作了")
-	log.Debug("初始化函数开始工作了")
+func logDefault() {
+	log = loging.Default()
+	log.Debug("使用默认配置创建")
+}
+
+func logConfig() {
+
+	file, err := os.OpenFile("./dame.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 644)
+	if err != nil {
+		file = os.Stdout
+	}
+
+	var config loging.Config
+	config.LogLeve = loging.Debug
+	config.LogFormat = loging.Json
+	config.TimeFormat = "2006-01-02 15:03:04"
+	config.LogCaller = true
+	config.LogOutput = []io.Writer{os.Stdout, file}
+
+	log = loging.NewLoging(&config)
+
+	log.Debug("使用配置文件创建")
+
 }
 
 func main() {
-	log = loging.Default()
-	log.Debug("hell")
-	log.Debug("你好！")
-	initF()
+	logDefault()
+	// logConfig()
 }
